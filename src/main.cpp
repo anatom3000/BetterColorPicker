229deac7269029717ee32aac00aed751fbbbd6e3
Anatom <49984698+anatom3000@users.noreply.github.com>
2024-11-05 07:44:36 +0100

    make the color picker interact with the game

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,21 @@ using namespace geode::prelude;
 const double PI = 3.1415926535897932384626433;
 const double TRIANGLE_SIZE = .85;
 
+using ColorChangedCallback = std::function<void(ccColor3B)>;
+
 class BetterColorPicker : public CCControl {
 public:
     float m_radius;
     CCPoint m_center;
+    bool m_touching;
     bool m_touchedHue;
     
     Ref<CCSprite> m_sprite;
     Ref<CCGLProgram> m_shader;
     Ref<CCSprite> m_hueNipple;
     Ref<CCSprite> m_svNipple;
+
+    ColorChangedCallback m_colorChangedCallback;
 
     // between 0.0 and 1.0
     double m_hue;
@@ -29,9 +34,9 @@ public:
     static CCPoint v2;
     static CCPoint v3;
 
-    static auto* create() {
+    static auto* create(ColorChangedCallback callback) {
         auto* node = new BetterColorPicker();
-        if (node->init()) {
+        if (node->init(callback)) {
             node->autorelease();
         } else {
             delete node;
@@ -41,10 +46,12 @@ public:
         return node;
     }
 
-    bool init() override {
+    bool init(ColorChangedCallback callback) {
         if (!CCControl::init()) return false;
         this->setTouchEnabled(true);
         this->registerWithTouchDispatcher();
+
+        m_colorChangedCallback = callback;
 
         m_sprite = CCSprite::create("frame.png"_spr);
         m_sprite->setCascadeOpacityEnabled(true);
@@ -61,21 +68,54 @@ public:
 
         this->m_hueNipple = CCSprite::create("nipple.png"_spr);
         this->m_svNipple = CCSprite::create("nipple.png"_spr);
+        m_hueNipple->setScale(0.8f);
+        m_svNipple->setScale(0.8f);
         
         this->addChild(m_hueNipple);
         this->addChild(m_svNipple);
 
         this->setContentSize(m_sprite->getContentSize());
 
-        this->updateHue(ccp(-1, 0));
-        this->updateSV(ccp(TRIANGLE_SIZE*m_radius*v2.x, TRIANGLE_SIZE*m_radius*v2.y));
-
         return true;
     }
 
-    void updateShader() {
+    void updateValues() {
         m_shader->setUniformLocationWith3f(m_shader->getUniformLocationForName("currentHsv"), m_hue, m_saturation, m_value);
         m_shader->use();
+
+        this->m_colorChangedCallback(this->getRgbValue());
+    }
+
+    ccColor3B getRgbValue() {
+        auto rgb = CCControlUtils::RGBfromHSV({.h = m_hue*360.0, .s = m_saturation, .v = m_value});
+
+        return ccc3(
+            (GLubyte)255.0*rgb.r,
+            (GLubyte)255.0*rgb.g,
+            (GLubyte)255.0*rgb.b
+        );
+    }
+
+    void setRgbValue(ccColor3B color) {
+        auto hsv = CCControlUtils::HSVfromRGB({
+            .r = color.r / 255.0f,
+            .g = color.g / 255.0f,
+            .b = color.b / 255.0f
+        });
+
+        m_hue = hsv.h / 360.0f;
+        m_saturation = hsv.s;
+        m_value = hsv.v;
+
+        double radius = m_radius * (1.0+TRIANGLE_SIZE) / 2.0;
+        double angle = (m_hue - 0.5) * 2 * PI;
+        double x = 1.0-m_value;
+        double y = m_saturation;
+        double z = 1.0-x-y;
+
+        m_hueNipple->setPosition(ccp(radius*cos(angle), radius*sin(angle)));
+        m_svNipple->setPosition(this->cartesianCoords(x, y, z) * ccp(TRIANGLE_SIZE * m_radius, TRIANGLE_SIZE * m_radius));
+        this->updateValues();
     }
 
     bool touchesHueWheel(CCPoint position) {
@@ -99,10 +139,53 @@ public:
         return {l1, l2, l3};
     }
 
+    double dot(CCPoint u, CCPoint v) {
+        return u.x*v.x + u.y*v.y;
+    }
+
+    // https://math.stackexchange.com/questions/1092912/find-closest-point-in-triangle-given-barycentric-coordinates-outside
+    std::tuple<double, double, double> closestPointInTriangle(double x, double y, double z) {
+        CCPoint p = cartesianCoords(x, y, z);
+
+        if (x >= 0 && y < 0) {
+            if (z < 0 && dot(p-v1, v2-v1) > 0) {
+                y = fmin(1.0, dot(p-v1, v2-v1) / dot(v2-v1, v2-v1));
+                z = 0.0;
+            } else {
+                y = 0.0;
+                z = clamp(dot(p-v1, v3-v1) / dot(v3-v1, v3-v1), 0.0, 1.0);
+            }
+
+            x = 1.0 - y - z;
+        } else if (y >= 0 && z < 0) {
+            if (x < 0 && dot(p-v2, v3-v2) > 0) {
+                z = fmin(1.0, dot(p-v2, v3-v2) / dot(v3-v2, v3-v2));
+                x = 0.0;
+            } else {
+                z = 0.0;
+                x = clamp(dot(p-v2, v1-v2) / dot(v1-v2, v1-v2), 0.0, 1.0);
+            }
+
+            y = 1.0 - z - x;
+        } else if (z >= 0 && x < 0) {
+            if (y < 0 && dot(p-v3, v1-v3) > 0) {
+                x = fmin(1.0, dot(p-v3, v1-v3) / dot(v1-v3, v1-v3));
+                y = 0.0;
+            } else {
+                x = 0.0;
+                y = clamp(dot(p-v3, v2-v3) / dot(v2-v3, v2-v3), 0.0, 1.0);
+            }
+
+            z = 1.0 - x - y;
+        }
+
+        return {x, y, z};
+    }
+
     CCPoint cartesianCoords(double x, double y, double z) {
         return ccp(
-            TRIANGLE_SIZE * m_radius * (x*v1.x + y*v2.x + z*v3.x),
-            TRIANGLE_SIZE * m_radius * (x*v1.y + y*v2.y + z*v3.y)
+            x*v1.x + y*v2.x + z*v3.x,
+            x*v1.y + y*v2.y + z*v3.y
         );
     }
 
@@ -122,19 +205,18 @@ public:
         double radius = m_radius * (1.0+TRIANGLE_SIZE) / 2.0;
 
         m_hueNipple->setPosition(ccp(radius*cos(angle), radius*sin(angle)));
-        this->updateShader();
+        this->updateValues();
     }
 
     void updateSV(CCPoint position) {
-        auto [x, y, z] = barycentricCoords(position);
-        x = clamp(x, 0.0, 1.0);
-        y = clamp(y, 0.0, 1.0);
-        z = clamp(z, 0.0, 1.0);
-        
+        auto [bx, by, bz] = barycentricCoords(position);
+        auto [x, y, z] = closestPointInTriangle(bx, by, bz);
+
         m_saturation = y;
         m_value = 1.0-x;
         
-        this->updateShader();
+        m_svNipple->setPosition(this->cartesianCoords(x, y, z) * ccp(TRIANGLE_SIZE * m_radius, TRIANGLE_SIZE * m_radius));
+        this->updateValues();
     }
 
     bool ccTouchBegan(CCTouch *touch, CCEvent *event) override {
@@ -142,17 +224,15 @@ public:
         
         if (this->touchesTriangle(position)) {
             this->updateSV(position);
+            this->m_touching = true;
             this->m_touchedHue = false;
             return true;
-        }
-
-
-        if (this->touchesHueWheel(position)) {
+        } else if (this->touchesHueWheel(position)) {
             this->updateHue(position);
+            this->m_touching = true;
             this->m_touchedHue = true;
             return true;
         }
-
 
         return false;
     }
@@ -165,6 +245,10 @@ public:
         } else {
             this->updateSV(position);
         }
+    }
+
+    void ccTouchEnded(CCTouch *touch, CCEvent *event) override {
+        this->m_touching = false;
     }
 
     void registerWithTouchDispatcher() override {
@@ -184,22 +268,53 @@ class $modify(MyColorSelectPopup, ColorSelectPopup) {
 	bool init(EffectGameObject* effect, cocos2d::CCArray* array, ColorAction* action) {
         if (!ColorSelectPopup::init(effect, array, action)) return false;
 
-        // TODO: add node IDs for ColorSelectPopup
-        m_colorPicker->setPosition(ccp(0, 1000));
+        m_colorPicker->setPosition(ccp(0, 10000));
 
-        m_fields->picker = BetterColorPicker::create();
+        m_fields->picker = BetterColorPicker::create([this](ccColor3B color) {
+            m_colorPicker->setColorValue(color);
+        });
         m_fields->picker->setPosition(ccp(284.f, 180.f) - m_buttonMenu->getPosition());
+        m_fields->picker->setRgbValue(m_colorPicker->getColorValue());
 
-        //menu->setPosition(ccp(284.f, 180.f));
         m_buttonMenu->addChild(m_fields->picker);
-        //
-
-        auto p = CCControlColourPicker::colourPicker();
-        p->setPosition(ccp(284.f, 180.f) - m_buttonMenu->getPosition());
-        //m_buttonMenu->addChild(p);
 
         return true;
 	}
+
+    // ColorSelectPopup::getColorValue and ColorSelectPopup::selectColor are inlined on Windows :/
+
+    void onDefault(CCObject* sender) {
+        ColorSelectPopup::onDefault(sender);
+        m_fields->picker->setRgbValue(m_colorPicker->getColorValue());
+    }
+
+    void onPaste(CCObject* sender) {
+        ColorSelectPopup::onPaste(sender);
+        m_fields->picker->setRgbValue(m_colorPicker->getColorValue());
+    }
+
+    void textChanged(CCTextInputNode* input) {
+        ColorSelectPopup::textChanged(input);
+        return;
+        
+        if (!m_fields->picker || m_fields->picker->m_touching) return;
+
+        std::string hex = input->getString();
+        
+        if (hex.size() == 6) {
+            m_fields->picker->setRgbValue(ColorSelectPopup::hexToColor(hex));
+        }
+    }
+
+    void onToggleHSVMode(CCObject* sender) {
+        ColorSelectPopup::onToggleHSVMode(sender);
+
+        if (!static_cast<CCMenuItemToggler*>(sender)->isOn()) {
+            m_fields->picker->setPosition(ccp(284.f, 10196.f) - m_buttonMenu->getPosition());
+        } else {
+            m_fields->picker->setPosition(ccp(284.f, 196.f) - m_buttonMenu->getPosition());
+        }
+    }
 };
 
 $on_mod(Loaded) {
